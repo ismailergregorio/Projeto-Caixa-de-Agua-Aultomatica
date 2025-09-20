80e03951926c72e67d8cbff69da15237572dca69
ismailergregorio <ismailergregorio6@gmail.com>
2025-09-20 16:14:47 -0300

    .

--- Recepitor RS480(ESP 8266)/src/main.cpp
+++ Recepitor RS480(ESP 8266)/src/main.cpp
@@ -1,73 +1,262 @@
 #include <Arduino.h>
 
 #include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
-#define RS485_RX D2   // GPIO4 — RO do módulo (dados recebidos)
-#define RS485_TX D1   // GPIO5 — DI do módulo (não usado aqui)
+const char *ssid = "Ismailer Gregorio Oi Fibra 2.4G";
+const char *password = "27270404";
+
+#define RS485_RX D1   // GPIO4 — RO do módulo (dados recebidos)
+#define RS485_TX D2   // GPIO5 — DI do módulo (não usado aqui)
 #define RS485_CTRL D5 // GPIO14 — DE + RE juntos
+
+#define LED_CAIXA_VASIA_SERVIDOR D7 // LED CAIXA VASIA
+#define LED_CAIXA_CHEIA_WIFI D8     // LED CAIXA CHEIA
+
+#define L2 D6 // LED MOTOR DESLIGADO/LIGADO
+
+#define b1 D0
 
 SoftwareSerial rs485Serial(RS485_RX, RS485_TX); // RX, TX
 
-void RecebimentoDeDados();
+String RecebimentoDeDados();
 
-String S1;
-String S2;
-String S3;
+unsigned long intervalo = 2000; // 1 segundo
+unsigned long ultimoTempo = 0;  // Armazena último tempo que o LED mudou
+                                // String dados;
+// Configurações do MQTT
+const char *mqtt_server = "192.168.100.5"; // IP do broker
+const int mqtt_port = 1883;
+const char *mqtt_user = "meuuser"; // usuário criado no Mosquitto
+const char *mqtt_pass = "1234";    // senha
 
-String dados;
+WiFiClient espClient;           // cliente WiFi
+PubSubClient client(espClient); // cliente MQTT
+
+unsigned long ultimoTempo_F = 0; // Armazena o último tempo que o LED mudou
+bool estadoLed = LOW;            // Estado atual do LED
+
+void piscaLed(int led)
+{
+  unsigned long tF = millis(); // Atualiza o tempo sempre
+
+  if (tF - ultimoTempo_F >= 250)
+  {
+    // Alterna o estado do LED
+    estadoLed = !estadoLed;
+    digitalWrite(led, estadoLed);
+
+    // Atualiza o último tempo
+    ultimoTempo_F = tF;
+  }
+}
+
+bool estadoMotor = false;      // estado do motor/LED
+bool stadoAnterior = HIGH;     // último estado do botão
+unsigned long ultimoDebounce = 0;
+const unsigned long debounceDelay = 50;
+
+void verificaBotao() {
+  bool stadoAtualBotao = digitalRead(b1);
+
+  // Se o botão mudou de estado, reinicia o debounce
+  if (stadoAtualBotao != stadoAnterior) {
+    ultimoDebounce = millis();
+  }
+
+  // Se passou tempo suficiente sem mudança, considera válido
+  if ((millis() - ultimoDebounce) > debounceDelay) {
+    // Detecta transição HIGH -> LOW (pressionado)
+    if (stadoAnterior == HIGH && stadoAtualBotao == LOW) {
+      estadoMotor = !estadoMotor; // alterna o estado
+      digitalWrite(L2, estadoMotor ? HIGH : LOW);
+      Serial.print("Motor: ");
+      Serial.println(estadoMotor ? "1" : "0");
+    }
+  }
+
+  stadoAnterior = stadoAtualBotao;
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  String r = "";
+  Serial.print("Mensagem recebida [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (unsigned int i = 0; i < length; i++)
+  {
+    r = r + (char)payload[i];
+    // Serial.println(r);
+  }
+  if (r == "teste")
+  {
+    digitalWrite(L2, HIGH);
+  }
+  Serial.println(r);
+}
+
+bool stadoAtualBotao = true;
+bool stadoLed = false;
+
+String Sensor1;
+String Sensor2;
+String Sensor3;
+String Motor;
+
+void inity()
+{
+  client.publish("sensor/SEN[1]", Sensor1.c_str());
+  client.publish("sensor/SEN[2]", Sensor2.c_str());
+  client.publish("sensor/SEN[3]", Sensor3.c_str());
+  client.publish("sensor/MOTOR", Motor.c_str());
+}
+int tentativa = 0;
+void reconnect()
+{
+  // Loop até reconectar
+  while (!client.connected())
+  {
+    Serial.print("Tentando conectar MQTT...");
+    // clientID pode ser qualquer nome único
+    if (client.connect("ESP32Client", mqtt_user, mqtt_pass))
+    {
+      Serial.println("conectado!");
+      digitalWrite(LED_CAIXA_VASIA_SERVIDOR, LOW);
+      client.subscribe("teste/topico");
+      client.subscribe("sensor/SEN[1]");
+      client.subscribe("sensor/SEN[2]");
+      client.subscribe("sensor/SEN[3]");
+      client.subscribe("sensor/MOTOR");
+    }
+    else
+    {
+      Serial.print("falhou, rc=");
+      Serial.print(client.state());
+      Serial.println(" Tentando de novo em 1s");
+      Serial.println("Tentativa:");
+      Serial.println(tentativa);
+      tentativa = tentativa + 1;
+      digitalWrite(LED_CAIXA_VASIA_SERVIDOR, HIGH);
+      delay(500);
+    }
+    if (tentativa >= 5)
+    {
+      break;
+    }
+  }
+}
+
+void ConexaoWifi()
+{
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+    piscaLed(LED_CAIXA_CHEIA_WIFI);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
 
 void setup()
 {
-  pinMode(RS485_CTRL, OUTPUT);
-  digitalWrite(RS485_CTRL, LOW); // Inicialmente em recepção
-  Serial.begin(115200);          // Para debug
+  pinMode(LED_CAIXA_VASIA_SERVIDOR, OUTPUT);
+  pinMode(LED_CAIXA_CHEIA_WIFI, OUTPUT);
 
-  rs485Serial.begin(9600); // Deve bater com o ESP
+  pinMode(L2, OUTPUT);
 
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  Serial.begin(115200);
+  ConexaoWifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+
+  pinMode(RS485_CTRL, OUTPUT);
+  digitalWrite(RS485_CTRL, LOW); // Inicialmente em recepção
+  rs485Serial.begin(9600);       // Deve bater com o arduino nano
+  delay(100);
+
+  digitalWrite(LED_CAIXA_VASIA_SERVIDOR, LOW);
+  digitalWrite(LED_CAIXA_CHEIA_WIFI, LOW);
+
+  pinMode(b1, INPUT_PULLUP);
 
   Serial.println("Recpitor Ativo!");
 }
 
 void loop()
 {
-  RecebimentoDeDados();
+  verificaBotao();
+  if (!client.connected() && tentativa <= 5)
+  {
+    reconnect();
+  }
+  client.loop();
+  String resultado;
+
+  // Publicar mensagem a cada 1s
+  static unsigned long lastMsg = 0;
+  if (millis() - lastMsg > 1000)
+  {
+    lastMsg = millis();
+    String msg = "Olá do ESP!";
+    client.publish("teste/topico", msg.c_str());
+    inity();
+  }
+
+  unsigned long tempoAtual = millis();
+  resultado = RecebimentoDeDados();
+  if (resultado.length() > 0)
+  {
+    ultimoTempo = tempoAtual;
+    digitalWrite(LED_BUILTIN, LOW);
+    // Serial.println(resultado);
+
+    if (resultado.startsWith("#SEN[1]"))
+    {
+      char c = resultado.charAt(8);
+      Sensor1 = c;
+    }
+
+    if (resultado.startsWith("#SEN[2]"))
+    {
+      char c = resultado.charAt(8);
+      Sensor2 = c;
+    }
+
+    if (resultado.startsWith("#SEN[3]"))
+    {
+      char c = resultado.charAt(8);
+      Sensor3 = c;
+    }
+  }
+  if (tempoAtual - ultimoTempo >= intervalo)
+  {
+    // Serial.println("Desconectado");
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+
 }
 
-void RecebimentoDeDados()
+String RecebimentoDeDados()
 {
+
+  String dados;
+
   if (rs485Serial.available())
   {
-    String dados = rs485Serial.readStringUntil('\n'); // Lê até o caractere '\n'
-
-    Serial.println("Dados Recebidos:");
-    Serial.println(dados);
-
-    // if (dados.startsWith("#SEN[1]:"))
-    // {
-    //   String valor = dados.substring(8);
-    //   Serial.println("dado recebido e do SEN[1] e " + valor);
-    //   S1 = valor;
-    //   int valorS1 = S1.toInt();
-    //   digitalWrite(LED_BUILTIN, valorS1);
-    // }
-    // else if (dados.startsWith("#SEN[2]:"))
-    // {
-    //   String valor = dados.substring(8);
-
-    //   Serial.println("dado recebido e do SEN[2] e " + valor);
-    //   S2 = valor;
-    // }
-    // else if (dados.startsWith("#SEN[3]:"))
-    // {
-    //   String valor = dados.substring(8);
-
-    //   Serial.println("dado recebido e do SEN[3] e " + valor);
-    //   S3 = valor;
-    // }
-    // else
-    // {
-    //   Serial.println("Comando desconhecido");
-    // }
+    dados = rs485Serial.readStringUntil('\n'); // Lê até o caractere '\n'
   }
+  return dados;
 }
